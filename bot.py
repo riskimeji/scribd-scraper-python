5b17752897f0f52c628233d83afbcc0fc9083375
riskimeji <meji.riski123@gmail.com>
2024-01-18 17:48:48 +0700

    fix

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ query_str = input("Kata Kunci: ")
 filter_str = input("Filter: ")
 query_str = query_str.replace(" ", "+")
 page = int(page_str)
+prefix_str = "https://www.scribd.com/document/"
 
 url = "https://www.scribd.com/search/query?query={}&page={}".format(query_str, page)
 
@@ -14,7 +15,7 @@ response = requests.get(url)
 if response.status_code == 200:
     data = response.json()
     data = data.get("results", {}).get("documents", {}).get("content", {}).get("documents",{})
-    documents_info = [{"id": doc["id"], "title": doc["title"]} for doc in data if filter_str.lower() in doc["title"].lower()]
+    documents_info = [{"url": prefix_str+str(doc["id"]), "title": doc["title"]} for doc in data if filter_str.lower() in doc["title"].lower()]
    
     with open('result.json', 'w') as file:
         json.dump(documents_info, file, indent=2)
