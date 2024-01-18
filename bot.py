5e1c7abf7950a694a78c1b50e6089562e35bf449
riskimeji <meji.riski123@gmail.com>
2024-01-18 17:29:09 +0700

    first commit

---
+++ bot.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+page_str = input("Page: ")
+query_str = input("Kata Kunci: ")
+filter_str = input("Filter: ")
+query_str = query_str.replace(" ", "+")
+page = int(page_str)
+
+url = "https://www.scribd.com/search/query?query={}&page={}".format(query_str, page)
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    data = data.get("results", {}).get("documents", {}).get("content", {}).get("documents",{})
+    documents_info = [{"id": doc["id"], "title": doc["title"]} for doc in data if filter_str.lower() in doc["title"].lower()]
+   
+    with open('result.json', 'w') as file:
+        json.dump(documents_info, file, indent=2)
+
+    print("Sukses simpan.")
+else:
+    print(f"Gagal mengambil data. Status code: {response.status_code}")
